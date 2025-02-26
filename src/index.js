f8282750e50e853f12f40a8eb13195fae0b9346c
cassi-r <cassinr@icloud.com>
2025-02-25 17:14:55 -0800

    added js to heighten user experience

--- src/index.js
+++ src/index.js
@@ -12,8 +12,12 @@ function generateRecipe(event) {
   let userInput = document.querySelector("#user-input");
   let apiKey = "eb2dt3eff4e3bdadf1b2o03ae3a5430d";
   let prompt = `User instructions: generate a recipe using ${userInput.value}`;
-  let context = `You are a world renowned chef who wants to create a recipe with the ${userInput.value}. The recipe should be easy to follow and not too time consuming. create a title, and list the ingredients under the word ingredients. seperate each line with <br />`;
+  let context = `You are a world renowned chef who wants to create a recipe with the ${userInput.value}. The recipe should be easy to follow and not too time consuming. create a title, and list the ingredients under the word ingredients. Seperate each line with <br>.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let recipeElement = document.querySelector("#ingredients");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class="generating">⏳ Generating a recipe with ${userInput.value}</div>`;
 
   axios.get(apiUrl).then(displayRecipe);
 }
